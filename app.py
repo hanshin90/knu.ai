552116dc9338eabefa8fea9fdb5cd3e88883c46f
hanshin90 <whiteworldhjk@gmail.com>
2025-12-02 14:25:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 import streamlit as st
+# import cv2
+from PIL import Image
 
-st.title("hello, kitty")
 
-st.write('good morning')
+st.title('Hello, kitty')
+st.write('this is for deplying at streamlit')
+
+
+img = Image.open("Lenna.png")
+
+st.image(img)
+
+
+
+@requirements.txt;
+
+streamlit
+opencv-python-headless
+numpy
+torch
